766ece55638a4afd71df05589104e66482d250e5
CasuallyCritical <onetruejdyt@gmail.com>
2022-09-30 14:08:05 -0400

    Add files via upload

---
+++ StateNode.java
@@ -0,0 +1,46 @@
+package Project_1;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+public class StateNode {
+
+    public StateNode parent;
+
+    public Vector<Integer> stateVector;
+
+    public ArrayList<StateNode> children = new ArrayList<StateNode>();
+
+    public int depth_check = 1;
+
+    public StateNode(int w, int s, int b) {
+        stateVector = new Vector<Integer>();
+        stateVector.add(0, w);
+        stateVector.add(1, s);
+        stateVector.add(2, b);
+    }
+
+    public int getWolves(){
+        return stateVector.get(0);
+    }
+
+    public int getSheep() {
+        return stateVector.get(1);
+    }
+
+    public int getBoat() {
+        return stateVector.get(2);
+    }
+
+    public void addVector(int w, int s, int b) {
+        stateVector.set(0, stateVector.get(0) + w);
+        stateVector.set(1, stateVector.get(1) + s);
+        stateVector.set(2, stateVector.get(2) + b);
+    }
+
+    public void addChild(StateNode child) {
+        children.add(child);
+        child.parent = this;
+    }
+
+}
