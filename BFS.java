34741ae97bf4d16aaa358e11e7e2921915eac8ff
CasuallyCritical <onetruejdyt@gmail.com>
2022-10-23 22:41:30 -0400

    Update BFS.java
    made it work

--- BFS.java
+++ BFS.java
@@ -1,54 +1,121 @@
-import java.util.ArrayList;
+package WolvesAndSheep;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Vector;
+ 
 class BFS {
-    Vector<Integer> stateVector;
-    BFS left, right;
-    public BFS(Vector item)
-    {
-        stateVector = item;
-        left = right = null;
-    }
-}
- 
-class BinaryTree {
     // Root of the Binary Tree
-    BFS root;
+    StateNode root;
  
-    public BinaryTree() { 
+    public BFS() { 
         root = null; //initialize it to empty
     }
  
     
 //height of the tree for LO traversal
-    int height(BFS root)
+    int height(StateNode root)
     {
         if (root == null)
             return 0;
         else {
             /* compute  height of each subtree */
-            int leftheight = height(root.left);
-            int rightheight = height(root.right);
+            Vector<Integer> heights = new Vector<Integer>();
+
+            for(StateNode child : root.children) {
+                heights.add(height(child));
+            }
  
             /* use the larger one */
-            if (leftheight > rightheight)
-                return (leftheight + 1);
-            else
-                return (rightheight + 1);
+            heights.sort(Collections.reverseOrder());
+
+            return heights.get(0) + 1;
         }
     }
- 
-    //add code to assemble vec into generic tree
-    //search via bfs sorta done above
-    void printCurrentLevel(Vector root, int level, Vector match)
-    {
-        if (root == null)
-            return;
-        if (level == 1)
-            System.out.print(root.get(level) + " ");
-        if(root == match ){
-            System.out.println("found a match, keeping above stack");
+
+    public StateNode generate() {
+        root = new StateNode(3, 3, 1);
+
+		//Key difference between BFS and DFS is that BFS uses a Queue instead of a Stack
+        Queue<StateNode> stateQueue = new LinkedList<StateNode>();
+
+        stateQueue.add(root);
+
+        StateNode success = null;
+
+        if(success == null) {
+            System.out.println("Checking tree...");
+            while(!stateQueue.isEmpty()) {
+                StateNode current = stateQueue.poll();
+    
+                /*
+                System.out.println("Testing DFS layer: " + current.depth_check);
+                System.out.println("Number of Children of node: " + current.children.size());
+                System.out.println("Wolves: " + current.getWolves() + "\nSheep: " + current.getSheep() + "\nBoat: " + current.getBoat());
+                //*/
+                    
+				
+				//Check to see if we found our winner
+                if(current.isSuccessCase()) {
+                    success = current;
+                    break;
+                }
+				
+				//If this node already has children, just add the new ones to the queue
+                if(current.children.isEmpty() == false) {
+                    for(StateNode n : current.children) {
+                        stateQueue.add(n);
+                    }
+                } else {
+                    // Possible Combinations:
+                        /*
+                         * (0, 1)
+                         * (1, 0)
+                         * (1, 1)
+                         * (0, 2)
+                         * (2, 0)
+                         * 
+                         */
+						 
+					// Because the boat has to go back and forth, we know that we have to subtract and add dependent on where the boat is
+                    if(current.getBoat() == 1) {
+                        int w = current.getWolves(), s = current.getSheep();
+    
+                        for(int x = 0; x < 3; x++) {
+                            for(int y = 0; y < 3; y++) {
+                                if(!(x == 0 && y == 0) && !(x + y > 2)) {
+                                    StateNode temp = new StateNode(w - x, s - y, 0);
+                                    if(temp.isFailCase() == false && root.searchForMatch(temp) == null && (temp.getWolves() <= 3 && temp.getWolves() >= 0) && (temp.getSheep() <= 3 && temp.getSheep() >= 0)) {
+                                        current.addChild(temp);
+                                    }
+                                }
+                            }
+                        }
+                    } else {
+                        int w = current.getWolves(), s = current.getSheep();
+    
+                        for(int x = 0; x < 3; x++) {
+                            for(int y = 0; y < 3; y++) {
+                                if(!(x == 0 && y == 0) && !(x + y > 2)) {
+                                    StateNode temp = new StateNode(w + x, s + y, 1);
+                                    if(temp.isFailCase() == false && root.searchForMatch(temp) == null && (temp.getWolves() <= 3 && temp.getWolves() >= 0) && (temp.getSheep() <= 3 && temp.getSheep() >= 0)) {
+                                        current.addChild(temp);
+                                    }
+                                }
+                            }
+                        }
+                    }
+    
+                    for(StateNode n : current.children) {
+                        stateQueue.add(n);
+                    }
+                }
+            }
+
+            stateQueue.add(root);
         }
+
+        return success;
     }
- 
 
 }
